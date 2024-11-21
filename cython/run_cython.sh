98fe7077365c017eccec6b33d61ac7dcb0646448
Luke Johnson <sa21722@bc4login2.bc4.acrc.priv>
2024-11-21 14:51:05 +0000

    slurm file, updated gitigore to not include output from BC4

--- cython/run_cython.sh
+++ cython/run_cython.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+# ======================
+# my_python_job.sh
+# ======================
 
-# # Delete all .c and .so files in the current directory
-# rm -rf build *.c *.so
+#SBATCH --job-name=cython_job           # Name of the job
+#SBATCH --partition=teach_cpu         # Use the teaching CPU partition
+#SBATCH --account=PHYS033184          # Account for Advanced Computational Physics
+#SBATCH --nodes=1                     # Use 1 node
+#SBATCH --ntasks-per-node=1           # Use 1 task per node
+#SBATCH --cpus-per-task=1             # Use 1 CPU per task
+#SBATCH --time=0:02:00                # Wall time (10 minutes for testing)
+#SBATCH --mem=1G                      # Memory allocation (1 GB)
 
 # Source the conda script to make conda command available
-source ~/miniconda3/etc/profile.d/conda.sh  # Adjust this path if needed for your Conda installation
+source ~/miniconda3/etc/profile.d/conda.sh
 
 # Activate the environment
 conda activate LB_env
+
+# Change to the submission directory
+cd $SLURM_SUBMIT_DIR
 
 setup_file=setup.py
 run_file=main.py
